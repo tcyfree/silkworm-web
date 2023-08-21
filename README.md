d79f6070206f08dfb9ffa44235a49fb240639d18
tangcy <bohr@icloud.com>
2023-08-21 19:56:08 +0800

    add bkg-img

--- README.md
+++ README.md
@@ -1,20 +1,3 @@
-# [Vue Material Kit 2]() 
-
-![version](https://img.shields.io/badge/version-2.0.0-blue.svg) [![GitHub issues open](https://img.shields.io/github/issues/creativetimofficial/vue-material-kit.svg)](https://github.com/creativetimofficial/vue-material-kit/issues?q=is%3Aopen+is%3Aissue) [![GitHub issues closed](https://img.shields.io/github/issues-closed-raw/creativetimofficial/vue-material-kit.svg)](https://github.com/creativetimofficial/vue-material-kit/issues?q=is%3Aissue+is%3Aclosed)
-
-![Image](https://s3.amazonaws.com/creativetim_bucket/products/97/original/vue-material-kit-2.jpg)
-
-
-- [VueJS 3](https://vuejs.org/) - An approachable, performant and versatile framework for building web user interfaces.
-- [Bootstrap 5](https://getbootstrap.com/) - Powerful, extensible, and feature-packed frontend toolkit.
-- [Popper.js](https://popper.js.org/) - Kickass library used to manage poppers
-- [Pinia](https://pinia.vuejs.org/) - The Vue Store that you will enjoy using.
-- [PrismJS](https://prismjs.com/) - A lightweight and extensible syntax highlighter.
-- [TypedJS](https://mattboldt.com/demos/typed-js/) - A JavaScript Typing Animation Library.
-- [Vue Clipboard 3](https://github.com/JamieCurnow/vue-clipboard3) - Easily copy to clipboard in Vue 3.
-- [Vue Count To](http://panjiachen.github.io/countTo/demo/) - A dependency-free, lightweight vue component for count up.
-
-
 ## 👩‍💻  [Quick Start](#quick-start)
 
 ```bash
@@ -27,6 +10,9 @@ npm dev or yarn run dev
 
 Navigate to https://localhost:3000
 
+[VueJS 3](https://vuejs.org/) 
+
+[Bootstrap 5](https://getbootstrap.com/)
 
 ## Browser Support
 
--- src/views/Presentation/PresentationView.vue
+++ src/views/Presentation/PresentationView.vue
@@ -20,7 +20,7 @@ import PresentationTestimonials from "./Sections/PresentationTestimonials.vue";
 import PresentationInformation from "./Sections/PresentationInformation.vue";
 
 //images
-import vueMkHeader from "@/assets/img/vue-mk-header.jpg";
+import vueMkHeader from "@/assets/img/silkworm.jpg";
 import wavesWhite from "@/assets/img/waves-white.svg";
 import logoBootstrap from "@/assets/img/logos/bootstrap5.jpg";
 import logoTailwind from "@/assets/img/logos/icon-tailwind.jpg";
@@ -62,11 +62,10 @@ onUnmounted(() => {
               class="text-white pt-3 mt-n5 me-2"
               :style="{ display: 'inline-block ' }"
             >
-              Material Kit 2
+              桑蚕疾病监测
             </h1>
             <p class="lead text-white px-5 mt-3" :style="{ fontWeight: '500' }">
-              Start the Development With Bootstrap 5 Design System inspired by
-              Material Design.
+              基于人工智能的工厂化养蚕生产线蚕病检测与防控服务系统
             </p>
           </div>
         </div>
@@ -76,8 +75,6 @@ onUnmounted(() => {
 
   <div class="card card-body blur shadow-blur mx-3 mx-md-4 mt-n6">
     <PresentationCounter />
-    <PresentationInformation />
-    <PresentationExample :data="data" />
     <PresentationPages />
     <BuiltByDevelopers />
 
@@ -258,36 +255,6 @@ onUnmounted(() => {
                 :style="{ height: '90px' }"
               />
             </a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="py-5">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-5 ms-auto">
-            <h4 class="mb-1">Thank you for your support!</h4>
-            <p class="lead mb-0">We deliver the best web products</p>
-          </div>
-          <div class="col-lg-5 me-lg-auto my-lg-auto text-lg-end mt-5">
-            <MaterialSocialButton
-              route="https://twitter.com/intent/tweet?text=Check%20Material%20Design%20System%20made%20by%20%40CreativeTim%20%23webdesign%20%23designsystem%20%23bootstrap5&url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fmaterial-design-system-pro"
-              component="twitter"
-              color="twitter"
-              label="Tweet"
-            />
-            <MaterialSocialButton
-              route="https://www.facebook.com/sharer/sharer.php?u=https://www.creative-tim.com/product/material-design-system-pro"
-              component="facebook-square"
-              color="facebook"
-              label="Share"
-            />
-            <MaterialSocialButton
-              route=""
-              component="pinterest"
-              color="pinterest"
-              label="Pin it"
-            />
           </div>
         </div>
       </div>
